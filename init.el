da459a9337d58e7c0e60f10bec0cc9ee6b005495
james@dyerdwelling.family <james@dyerdwelling.family>
2025-02-23 14:23:06 +0000

    Update keybindings: remove unnecessary `M-s g` shortcut.

--- init.el
+++ init.el
@@ -91,7 +91,6 @@
 (global-set-key (kbd "M-H") #'my/mark-line)
 (global-set-key (kbd "M-s =") #'ediff-buffers)
 (global-set-key (kbd "M-s +") #'ediff-regions-linewise)
-(global-set-key (kbd "M-s g") #'my/grep)
 (global-set-key (kbd "M-s h") #'my/mark-block)
 (global-set-key (kbd "M-h") #'my/mark-block)
 (global-set-key (kbd "M-s j") #'eval-defun)
