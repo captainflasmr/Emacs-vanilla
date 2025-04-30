8be145a100892d96609d8d634bc315bd10a3b712
james dyer <captainflasmr@gmail.com>
2025-04-30 16:01:19 +0100

    Removed old mark block keybinding to make way for highlighting!

--- init.el
+++ init.el
@@ -93,7 +93,6 @@
 (global-set-key (kbd "M-H") #'my/mark-line)
 (global-set-key (kbd "M-s =") #'ediff-buffers)
 (global-set-key (kbd "M-s +") #'ediff-regions-linewise)
-(global-set-key (kbd "M-s h") #'my/mark-block)
 (global-set-key (kbd "M-h") #'my/mark-block)
 (global-set-key (kbd "M-s j") #'eval-defun)
 (global-set-key (kbd "M-s x") #'diff-buffer-with-file)
