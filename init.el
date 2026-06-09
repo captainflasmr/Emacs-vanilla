2e2b3beb950dc8d3d83c07d4db0733c6fab06d76
james dyer <captainflasmr@gmail.com>
2026-06-09 18:44:36 +0100

    ensure utF-8 is used

--- init.el
+++ init.el
@@ -392,6 +392,16 @@
 (setq ring-bell-function 'ignore)
 
 ;;
+;; -> coding-core
+;;
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+
+;;
 ;; -> setqs-core
 ;;
 (setq mouse-highlight nil)
